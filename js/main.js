e40fc624a7cf3fea5cb9cd3a63f4861b9582b3c3
Ali Abdel-Baki <aliabdelbaky@bdaya-dev.com>
2020-05-04 14:01:31 +0200

    OmniFood

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+   'use strict';
+    var winH = $(window).height(),
+        lo = $(".darkLogo").innerHeight(),
+    img = $(".images").innerHeight() ;
+    $(".header , .overlay").height(winH);
+});
